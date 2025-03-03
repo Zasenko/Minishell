a60f7ebaafd8cb7c24136570f96047df59354a08
Dmitry Zasenko <dzasenko@c2r2p4.42vienna.com>
2025-03-03 15:19:58 +0100

    fixing exit

--- src/builtins/exit.c
+++ src/builtins/exit.c
@@ -6,31 +6,168 @@
 /*   By: dzasenko <dzasenko@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/24 09:59:49 by dzasenko          #+#    #+#             */
-/*   Updated: 2025/02/27 17:31:54 by dzasenko         ###   ########.fr       */
+/*   Updated: 2025/03/03 15:17:59 by dzasenko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
+
+int arr2d_len(char **arr)
+{
+    int i = 0;
+    if (!arr)
+        return 0;
+    while (arr[i])
+        i++;
+    return (i);
+}
+
+// int check_exit_arg(char *arg)
+// {
+//     int i;
+//     char *trimmed;
+    
+//     i = 0;
+//     trimmed = ft_strtrim(arg, " \t");
+//     if (!trimmed)
+//         return (1);
+            
+//     if (trimmed[i] == '+' || trimmed[i] == '-')
+//          i++;
+//     if (ft_strlen(&trimmed[i]) < 1)
+//     {
+//         ft_putstr_fd("exit: ", 2);
+//         ft_putstr_fd(arg, 2);
+//         ft_putstr_fd(": numeric argument required\n", 2);
+//         free(trimmed);
+//         return (2);
+//     }
+//     while (trimmed[i])
+//     {
+//         if (!ft_isdigit(trimmed[i]))
+//         {
+//             ft_putstr_fd("exit: ", 2);
+//             ft_putstr_fd(arg, 2);
+//             ft_putstr_fd(": numeric argument required\n", 2);
+//             free(trimmed);
+//             return (2);
+//         }
+//         i++;
+//     }
+//     free(trimmed);
+//     return (0);
+// }
+
+// int check_exit_args(char **args, t_app *shell)
+// {
+//     int i;
+
+//     i = 1;
+//     while (args[i])
+//     {
+//         int result = check_exit_arg(args[i]);
+//         if (result != 0)
+//         {
+//             if (result == 1)
+//             {
+//                 free_list(shell);
+//                 exit(1);
+//             }
+//             shell->last_exit_code = result;
+//             return 1;
+//         }
+//         i++;        
+//     }
+// }
 
 int ft_exit(t_cmd *cmd, t_app *shell, int is_parent)
 {
     int exit_code;
 
     exit_code = 0;
-    
-    if (cmd->args[1] != NULL)
+
+    if (arr2d_len(cmd->args) > 2)
     {
-        // todo check numbers and +- only
-        exit_code = ft_atoi(cmd->args[1]);
+        ft_putstr_fd("exit: too many arguments\n", 2);
+        
+        shell->last_exit_code = 130;
+        return 130;
     }
 
-    if (is_parent)
+    else if (arr2d_len(cmd->args) == 1)
     {
-        // free_envp_list(&shell->envp);
-        free_list(shell);
         printf("exit\n");
-        exit(exit_code);
+        if (is_parent)
+        {
+            // free_envp_list(&shell->envp);
+            free_list(shell);
+            exit(shell->last_exit_code);
+        }
+        else
+            exit(shell->last_exit_code);
     }
     else
-        exit(exit_code);
+    {
+        char *trimmed = ft_strtrim(cmd->args[1], " \t");  
+        if (!trimmed)
+        {
+            if (is_parent)
+            {
+                // free_envp_list(&shell->envp);
+                free_list(shell);
+                exit(1);
+            }
+            else
+                exit(1);
+        }
+        int i = 0;
+        if (trimmed[i] == '+' || trimmed[i] == '-')
+            i++;
+        if (ft_strlen(&trimmed[i]) < 1)
+        {
+            ft_putstr_fd("exit: ", 2);
+            ft_putstr_fd(cmd->args[1], 2);
+            ft_putstr_fd(": numeric argument required\n", 2);
+            // printf("exit\n");
+            // printf("bash: exit: %s: numeric argument required\n", trimmed);
+            free(trimmed);
+            if (is_parent)
+            {
+                free_list(shell);
+                exit(2);
+            }
+            else
+                exit(2);
+        }
+
+        while (trimmed[i])
+        {
+            if (!ft_isdigit(trimmed[i]))
+            {
+            ft_putstr_fd("exit: ", 2);
+            ft_putstr_fd(cmd->args[1], 2);
+            ft_putstr_fd(": numeric argument required\n", 2);
+                free(trimmed);
+                if (is_parent)
+                {
+                    free_list(shell);
+                    exit(2);
+                }
+                else
+                    exit(2);
+                }
+            i++;
+        }
+        exit_code = ft_atoi(trimmed);
+        free(trimmed);
+        printf("exit\n");
+        if (is_parent)
+        {
+            free_list(shell);
+            exit(exit_code);
+        }
+        else
+            exit(exit_code);
+    }
+
 }
--- src/executor/executor.c
+++ src/executor/executor.c
@@ -1,13 +1,12 @@
-<<<<<<< HEAD
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   executor.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ibondarc <ibondarc@student.42.fr>          +#+  +:+       +#+        */
+/*   By: dzasenko <dzasenko@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/11 16:49:29 by dmitryzasen       #+#    #+#             */
-/*   Updated: 2025/03/02 16:28:00 by ibondarc         ###   ########.fr       */
+/*   Updated: 2025/03/03 11:21:16 by dzasenko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -77,7 +76,7 @@ void	child_process(t_app *shell, t_cmd *cmd, int prev_pipe, int pipe_fd[2])
 	else
 	{
 		execve(cmd->cmd, cmd->args, shell->env_var);
-		exit_with_error(shell, errno, "hello error!");
+		exit_with_error(shell, errno, strerror(errno));
 	}
 }
 
@@ -129,7 +128,8 @@ int	ft_wait_child(t_cmd *cmd, t_app *shell)
 	child_pid = waitpid(cmd->pid, &status, 0);
 	if (child_pid == -1)
 	{
-		return (perror("waitpid"), errno);
+		shell->last_exit_code = WEXITSTATUS(status);
+		return (strerror(errno), errno);
 	}
 	if (WIFEXITED(status))
 	{
@@ -225,232 +225,4 @@ int	ft_execute(t_app *shell)
 	if (prev_pipe != -1)
 		close(prev_pipe);
 	return (1);
-=======
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   executor.c                                         :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/02/11 16:49:29 by dmitryzasen       #+#    #+#             */
-/*   Updated: 2025/03/02 16:28:00 by marvin           ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
-#include "../../includes/minishell.h"
-
-int	close_all_cmnds_fds(t_cmd *cmd)
-{
-	t_cmd	*temp;
-
-	temp = cmd;
-	if (!temp)
-		return (0);
-	while (temp != NULL)
-	{
-		if (temp->fd_in != -1)
-		{
-			close(temp->fd_in);
-		}
-		if (temp->fd_out != -1)
-		{
-			close(temp->fd_out);
-		}
-		temp = temp->next;
-	}
-	return (1);
-}
-
-int	redirect(t_app *shell, t_cmd *cmd, int prev_pipe, int pipe_fd[2])
-{
-	if (prev_pipe != -1)
-	{
-		dup2(prev_pipe, 0);
-		close(prev_pipe);
-	}
-	if (cmd->fd_in != -1)
-	{
-		dup2(cmd->fd_in, 0);
-		close(cmd->fd_in);
-	}
-	if (cmd->next != NULL)
-	{
-		dup2(pipe_fd[1], 1);
-	}
-	if (cmd->fd_out != -1)
-	{
-		dup2(cmd->fd_out, 1);
-		close(cmd->fd_out);
-	}
-	if (pipe_fd[0] != -1)
-		close(pipe_fd[0]);
-	if (pipe_fd[1] != -1)
-		close(pipe_fd[1]);
-	close_all_cmnds_fds(shell->cmd);
-	return (1);
-}
-
-void	child_process(t_app *shell, t_cmd *cmd, int prev_pipe, int pipe_fd[2])
-{
-	int exit_status;
-
-	redirect(shell, cmd, prev_pipe, pipe_fd);
-	if (is_builtin_func(cmd->cmd))
-	{
-		exit_status = exec_buildin(cmd, shell);
-		exit(exit_status);
-	}
-	else
-	{
-		execve(cmd->cmd, cmd->args, shell->env_var);
-		exit_with_error(shell, errno, "hello error!");
-	}
-}
-
-int	ft_execute_command(t_app *shell, t_cmd *cmd, int *prev_pipe)
-{
-	int	pipe_fd[2];
-
-	pipe_fd[0] = -1;
-	pipe_fd[1] = -1;
-	if (cmd->next != NULL)
-	{
-		if (pipe(pipe_fd) != 0)
-		{
-			// check
-			perror("pipe");
-			return (0); // todo? break and waitpid?
-		}
-	}
-	cmd->pid = fork();
-	if (cmd->pid < 0)
-	{
-		perror("fork");
-		return (0); // todo? break and waitpid?
-	}
-
-	
-	// child
-	if (cmd->pid == 0)
-		child_process(shell, cmd, *prev_pipe, pipe_fd);
-	
-	if (cmd->fd_in != -1)
-		close(cmd->fd_in);
-	if (cmd->fd_out != -1)
-		close(cmd->fd_out);
-
-	if (*prev_pipe != -1)
-		close(*prev_pipe);
-	if (pipe_fd[1] != -1)
-		close(pipe_fd[1]);
-	*prev_pipe = pipe_fd[0];
-	return (1);
-}
-
-int	ft_wait_child(t_cmd *cmd, t_app *shell)
-{
-	int		status;
-	pid_t	child_pid;
-
-	child_pid = waitpid(cmd->pid, &status, 0);
-	if (child_pid == -1)
-	{
-		return (perror("waitpid"), errno);
-	}
-	if (WIFEXITED(status))
-	{
-		shell->last_exit_code = WEXITSTATUS(status);
-		return (SUCCESS);
-	}
-	return (SUCCESS);
-}
-
-int ft_wait_children(t_app *shell)
-{
-	t_cmd	*cmd;
-
-	cmd = shell->cmd;
-	while (cmd != NULL)
-	{
-		if (cmd->pid != -1)
-		{
-			ft_wait_child(cmd, shell);
-		}
-		cmd = cmd->next;
-	}
-	return (1);
-}
-
-int	ft_execute(t_app *shell)
-{
-	t_cmd	*cmd;
-	int		cmd_count;
-	int		prev_pipe;
-
-	cmd = shell->cmd;
-	cmd_count = cmd_len(shell->cmd);
-	prev_pipe = -1;
-	if (!cmd_count)
-		return (0);
-	while (cmd != NULL)
-	{
-		if (cmd->input != NULL)
-		{
-			int fd_in = open(cmd->input, O_RDONLY);
-			if (fd_in < 0)
-			{
-				perror("open");
-			}
-			else
-			{
-				cmd->fd_in = fd_in;
-			}
-		}
-		if (cmd->output != NULL)
-		{
-			int fd_out = open(cmd->output, O_WRONLY | O_CREAT |  O_TRUNC, 0644);
-			if (fd_out < 0)
-			{
-				perror("open");
-			}
-			else
-			{
-				cmd->fd_out = fd_out;
-			}
-		}
-		cmd = cmd->next;
-	}
-	cmd = shell->cmd;
-	
-	if (cmd->next == NULL && ft_strstr(cmd->cmd, "cd"))
-	{
-		shell->last_exit_code = ft_cd(cmd, shell->env_var);
-	}
-	else if (cmd->next == NULL && ft_strstr(cmd->cmd, "exit"))
-	{
-		ft_exit(cmd, shell, 1);
-	}
-	else if (cmd->next == NULL && ft_strstr(cmd->cmd, "export"))
-	{
-		shell->last_exit_code = ft_export(cmd, shell, false);
-	}
-	else if (cmd->next == NULL && ft_strstr(cmd->cmd, "unset"))
-	{
-		shell->last_exit_code = ft_unset(cmd, shell, false);
-	}
-	else {
-		while (cmd != NULL)
-		{
-			ft_execute_command(shell, cmd, &prev_pipe);
-			cmd = cmd->next;
-		}
-	}
-
-	ft_wait_children(shell);
-	close_all_cmnds_fds(shell->cmd);
-	if (prev_pipe != -1)
-		close(prev_pipe);
-	return (1);
->>>>>>> var_pars
 }
