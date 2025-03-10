e30e9f4d3b40916c940214086176994ffde508be
IgoryanDeltoro <bondatchuk1989@gmail.com>
2025-03-10 18:49:57 +0100

    fix

--- src/prompt/read_input_line.c
+++ src/prompt/read_input_line.c
@@ -102,7 +102,7 @@ void read_input_line(t_app *shell)
             parse_tokens(shell);
             ft_execute(shell);
             // print_tokens(shell);
-            print_cmd(&shell);
+            // print_cmd(&shell);
             // print_envp(shell->envp);
             free_list(shell);
         }
