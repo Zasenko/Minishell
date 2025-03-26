1eb48f120631db4c39e10be85f0726fb275404a8
IgoryanDeltoro <ibondarc@c3r2p6.42vienna.com>
2025-03-26 12:27:34 +0100

    fixed syntaxis errors

--- src/lexer/lexing_checker.c
+++ src/lexer/lexing_checker.c
@@ -12,36 +12,51 @@
 
 #include "../../includes/minishell.h"
 
-bool check_pipe(t_token *token)
+bool check_pipe(t_app *shell, t_token *token)
 {
     if (token->type == PIPE)
     {
-        if (token->next == NULL || token->prev == NULL 
-            ||  token->value == token->next->value )
+        if (token->next == NULL)
+        {
+            ft_putstr_fd("syntax error near unexpected token `newline", 2);
+            ft_putstr_fd("'\n", 2);
+            shell->last_exit_code = 2;
             return false;
+        }
+        else if (token->value == token->next->value || token->next->type != ARG )
+        {
+            ft_putstr_fd("syntax error near unexpected token `", 2);
+            if (token->next)
+                ft_putstr_fd(token->next->value, 2);
+            ft_putstr_fd("'\n", 2);
+            shell->last_exit_code = 2;
+            return false;
+        }
     }
     return true;
 }
 
-bool check_rederections(t_token *token)
+bool check_rederections(t_app *shell, t_token *token)
 {
-    if (token->type == REDIR_IN)
+    if (token->type == REDIR_IN || token->type == REDIR_OUT 
+        || token->type == APPEND || token->type == HEREDOC)
     {
-        if (token->next == NULL || token->value == token->next->value
-            || token->next->type != ARG )
+        if (token->next == NULL)
+        {
+            ft_putstr_fd("syntax error near unexpected token `newline", 2);
+            ft_putstr_fd("'\n", 2);
+            shell->last_exit_code = 2;
             return false;
-    }
-    else if (token->type == REDIR_OUT)
-    {
-        if (token->next == NULL || token->value == token->next->value
-            || token->next->type != ARG)
+        }
+        else if (token->value == token->next->value || token->next->type != ARG )
+        {
+            ft_putstr_fd("syntax error near unexpected token `", 2);
+            if (token->next)
+                ft_putstr_fd(token->next->value, 2);
+            ft_putstr_fd("'\n", 2);
+            shell->last_exit_code = 2;
             return false;
-    }
-    else if (token->type == APPEND)
-    {
-        if (token->next == NULL || token->value == token->next->value
-            || token->next->type != ARG)
-            return false;
+        }
     }
     return true;
 }
@@ -55,19 +70,18 @@ bool lexing_checker(t_app *shell)
     token = shell->tokens;
     while (token != NULL)
     {
-        if (!check_pipe(token))
+        if (!check_pipe(shell, token))
 		{
-            print_message(PIPE_ERROR, false);
             return false;
         }
-        if (!check_rederections(token))
+        if (!check_rederections(shell, token))
 		{
-            ft_putstr_fd("syntax error near unexpected token `", 2);
-            ft_putstr_fd(token->next->value, 2);
-            ft_putstr_fd("'\n", 2);
+            // ft_putstr_fd("syntax error near unexpected token `", 2);
+            // if (token->next)
+            //     ft_putstr_fd(token->next->value, 2);
+            // ft_putstr_fd("'\n", 2);
             // printf("EEEEEE: %s\n", token->value);
             // print_message(REDIR_ERROR, false);
-            shell->last_exit_code = 2;
             return false;
         }
         token = token->next;
