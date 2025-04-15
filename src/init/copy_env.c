71102aa8cb598aee83ed1d3b1b557eab4f8d83c7
IgoryanDeltoro <ibondarc@c3r2p6.42vienna.com>
2025-04-15 10:04:34 +0200

    env list refactor

--- src/init/copy_env.c
+++ src/init/copy_env.c
@@ -12,56 +12,70 @@
 
 #include "../../includes/minishell.h"
 
-void	copy_env(t_app *shell, char **envp)
+bool	reduce_shell_level(char *name, char *env_var)
 {
-	int i;
+	int level;
+
+	if (!name)
+		return (false);
+	if (!ft_strcmp(name, "SHLVL"))
+	{
+		level = ft_atoi(env_var);
+		level++;
+		free(env_var);
+		env_var = ft_itoa(level);
+		if (!env_var)
+			return (false);
+	}
+	return (true);
+}
+
+bool	create_env_copy(t_app *shell, char **envp, int *i)
+{
 	int j;
 	char *name;
 	char *env_var;
 	char *s;
 
-	i = 0;
+	while (envp[*i] != NULL)
+	{
+		j = 0;
+		s = envp[*i];
+		while (s[j] != '=')
+			j++;
+		env_var = ft_strdup(ft_strchr(envp[*i], '=', true));
+		if (!env_var)
+			return (false);
+		name = ft_substr(s, 0, j);
+		if (!name)
+			return (false);
+		if (!reduce_shell_level(name, env_var))
+			return (false);
+		add_envp_back(&shell->envp, create_new_envp(env_var, name));
+		(*i)++;
+	}
+	return (true);
+}
 
+void	copy_env(t_app *shell, char **envp)
+{
+	int i;
+	char buf[MAXPATHLEN];
+	char *dir;
+
+	if (!shell)
+		return ;
 	if (envp && envp[0])
 	{
-		while (envp[i] != NULL)
-		{
-			j = 0;
-			s = envp[i];
-			while (s[j] != '=')
-				j++;
-			env_var = ft_strdup(ft_strchr(envp[i], '=', true));
-			if (!env_var)
-				break ;
-			name = ft_substr(s, 0, j);
-			if (!name)
-				break ;
-
-            if (!ft_strcmp(name, "SHLVL"))
-            {
-                int i = ft_atoi(env_var);
-                //todo
-                i++;
-                free(env_var);
-                env_var = ft_itoa(i);
-                //todo
-            }
-
-			add_envp_back(&shell->envp, create_new_envp(env_var, name));
-			i++;
-		}
+		i = 0;
+		if (!create_env_copy(shell, envp, &i))
+			exit_with_error(shell, 1, MALLOC_FAIL);
 	}
-    else
-    {
-        char buf[MAXPATHLEN];
-        char *dir;
-    
-        dir = NULL;
-        dir = getcwd(buf, MAXPATHLEN);
-        // TODO
-        // if (!dir)
-        //     return (perror("getcwd"), errno);
-        add_envp_back(&shell->envp, create_new_envp(dir, "PWD"));
-        add_envp_back(&shell->envp, create_new_envp("1", "SHLVL"));
-    }
+	else
+	{
+		dir = NULL;
+		dir = getcwd(buf, MAXPATHLEN);
+		add_envp_back(&shell->envp, create_new_envp(dir, "PWD"));
+		add_envp_back(&shell->envp, create_new_envp("1", "SHLVL"));
+	}
 }
