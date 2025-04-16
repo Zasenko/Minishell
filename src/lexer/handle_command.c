167a7a2e253e2d9c3897682d58f96165c5bbf493
IgoryanDeltoro <ibondarc@c3r2p6.42vienna.com>
2025-04-16 16:19:41 +0200

    refactor command part

--- src/lexer/handle_command.c
+++ src/lexer/handle_command.c
@@ -12,26 +12,10 @@
 
 #include "../../includes/minishell.h"
 
-bool is_possible_expand(char *input)
+void write_value(t_token *token, char *value, int type)
 {
-    int  i = 0;
-    bool is_dq_open = false;
-    bool is_sq_open = false;
-
-    if (!input)
-        return false;
-
-    while (input[i])
-    {
-        if (input[i] == '\"' && !is_sq_open)
-            is_dq_open = !is_dq_open;
-        else if (input[i] == '\'' && !is_dq_open)
-            is_sq_open = !is_sq_open;
-        else if (input[i] == '$' && !is_sq_open)
-            return true;
-        i++;
-    }
-    return false;
+    token->value = value;
+    token->type = type;
 }
 
 char *divide_into_parts(char *input, int *i)
@@ -47,7 +31,7 @@ char *divide_into_parts(char *input, int *i)
             is_sq_open = !is_sq_open;
         else if (input[*i] == '\"' && !is_sq_open) 
             is_dq_open = !is_dq_open;
-        else if ((input[*i] == ' ' || input[*i] == '\t' || input[*i] == '>'  // to do >> <<
+        else if ((input[*i] == ' ' || input[*i] == '\t' || input[*i] == '>'  
             || input[*i] == '<' || input[*i] == '|') && !is_dq_open && !is_sq_open)
             break;
         (*i)++;
@@ -55,30 +39,39 @@ char *divide_into_parts(char *input, int *i)
     return ft_substr(input, start, *i - start);
 }
 
-int join_partitions(t_app *shell, char **dest, char *input)
+void skip_all_beafor_variable(char *input, int *j)
+{
+    bool    sing_quote;
+    bool    doub_quote;
+
+    sing_quote = false;
+    doub_quote = false;
+    while (input[*j])
+    {
+        if (input[*j] == '\'' && !doub_quote)
+            sing_quote = !sing_quote;
+        if (input[*j] == '\"' && !sing_quote)
+            doub_quote = !doub_quote;
+        if (input[*j] == '$' && !sing_quote)
+            break;
+        (*j)++;
+    }
+}
+
+int join_partitions(t_app *shell, char **dest, char *input, bool *do_split)
 {
     char    *expanded;
     char    *temp;
     int     j = 0;
-    bool    do_split = false;
     int     start;
-    bool    sing_quote = false;
-    bool    doub_quote = false;
 
-    *dest = ft_strdup(""); 
+    *dest = ft_strdup("");
+    if (!(*dest))
+        return false;
     while (input[j])
     {
         start = j; 
-        while (input[j])
-        {
-            if (input[j] == '\'' && !doub_quote)
-                sing_quote = !sing_quote;
-            if (input[j] == '\"' && !sing_quote)
-                doub_quote = !doub_quote;
-            else if (input[j] == '$' && !sing_quote)
-                break;
-            j++;
-        }
+        skip_all_beafor_variable(input, &j);
         if (start != j)
         {
             temp = ft_strjoin(*dest, ft_substr(input, start, j - start));
@@ -88,108 +81,118 @@ int join_partitions(t_app *shell, char **dest, char *input)
         if (input[j] == '$')
         {
             expanded = expand_words(shell, input, &j);
+            if (!expanded)
+                return false;
             if (ft_strchr(expanded, ' ', false))
-                do_split = !do_split;
+                *do_split = !(*do_split);
             temp = ft_strjoin(*dest, expanded);
             free(*dest);
             *dest = temp;
         }
     }
-    return do_split;
+    return true;
+}
+bool handle_redir_outfile(t_app *shell, t_token *token, char *input, bool *do_split)
+{
+    int     j;
+    char    **temp;
+    char    *result;
+
+    j = 0;
+    result = NULL;
+    if (!join_partitions(shell, &result, input, do_split))
+        return false;
+    temp = ft_split(result, ' ');
+    if (!temp)
+        return false;
+    while (temp[j])
+        j++;
+    if (j == 0 || j > 1 || !ft_strlen(temp[0]))
+    {
+        ft_putstr_fd(input, 2);
+        ft_putstr_fd(": ambiguous redirect\n", 2);
+        shell->last_exit_code = 1;
+        free(input);
+        return false;
+    } 
+    else
+        token->value =  result;
+    token->type = ARG;
+    return true;
+}
+
+bool add_expanded_value_into_node(t_token *token, char **input)
+{
+    int     j;
+    t_token *new;
+
+    j = 0;
+    if (!input)
+        return false;
+    while (input[j])
+    {
+        if (j == 0)
+            write_value(token, input[j], ARG);
+        else
+        {
+            new = create_new_token();
+            if (!new)
+                return false;
+            write_value(new, input[j], ARG);
+            add_token_back(&token, new);     
+        }
+        j++;
+    }
+    return true;
+} 
+bool handle_quotes_case(t_app *shell, t_token *token, char *part)
+{
+    bool    do_split;
+    char    **temp;
+
+    do_split = false;
+    if (define_valid_string(part))
+    {
+        if (!join_partitions(shell, &token->value, part, &do_split))
+            return false;
+        if (do_split)
+        {
+            temp = ft_split(token->value, ' ');
+            if (!add_expanded_value_into_node(token, temp))
+                return false;
+        }
+    }
+    else
+    {
+        token->value = ft_strdup(part);
+        if (!token->value) 
+            return false;
+    }
+    token->type = ARG;
+    return true;
 }
 
 bool handle_command(t_app *shell, t_token *token, char *input, int *i)
 {
-    char    *part;
-    char    **temp;
-    char    *result = NULL;
-    int     j;
+    char *part;
+    char *result;
+    bool    do_split;
 
+    do_split = false;
+    result = NULL;
     part = divide_into_parts(input, i);
     if (!part) 
-        return NULL;
+        return false;
     if (!ft_strchr(part, '$', false))
-    {
-        token->value = part;
-        token->type = ARG;
-        return true;
-    }
+        return (write_value(token, part, ARG), true);
     if (ft_strchr(part, '\"', false) || ft_strchr(part, '\'', false))
-    {
-        if (is_possible_expand(part))
-        {
-            bool do_split = join_partitions(shell, &token->value, part);
-            if (do_split)
-            {
-                temp = ft_split(token->value, ' ');
-                j = 0;
-                while (temp[j])
-                {
-                    if (j == 0)
-                    {
-                        token->value = temp[j];
-                        token->type = ARG;
-                    }
-                    else
-                    {
-                        t_token *new = create_new_token();
-                        new->value = temp[j];
-                        new->type = ARG;
-                        add_token_back(&token, new);     
-                    }
-                    j++;
-                }
-            }
-        }
-        else
-        {
-            token->value = ft_strdup(part);
-            if (!token->value)
-                return false;
-        }
-        token->type = ARG;
-    }
-    else if (token->prev && token->prev->type == REDIR_OUT)
-    {
-        join_partitions(shell, &result, part);
-        temp = ft_split(result, ' ');
-        j = 0;
-        while (temp[j])
-            j++;
-        if (j == 0 || j > 1 || !ft_strlen(temp[0]))
-        {
-            ft_putstr_fd(part, 2);
-            ft_putstr_fd(": ambiguous redirect\n", 2);
-            shell->last_exit_code = 1;
-            free(part);
-            return false;
-        } 
-        else
-            token->value =  result;
-        token->type = ARG;
-    }
-    else
-    {
-        join_partitions(shell, &result, part);
-        temp = ft_split(result, ' ');
-        j = 0;
-        while (temp[j])
-        {
-            if (j == 0)
-            {
-                token->value = temp[j];
-                token->type = ARG;
-            }
-            else
-            {
-                t_token *new = create_new_token();
-                new->value = temp[j];
-                new->type = ARG;
-                add_token_back(&token, new);     
-            }
-           j++;
-        }
-    }
+        return handle_quotes_case(shell, token, part);
+    if (token->prev && token->prev->type == REDIR_OUT)
+        return handle_redir_outfile(shell, token, part, &do_split);
+    if (!join_partitions(shell, &result, part, &do_split))
+        return false;
+    if (!add_expanded_value_into_node(token, ft_split(result, ' ')))
+        return false;
     free(part);
     return true;
 }
--- src/lexer/handle_variable.c
+++ src/lexer/handle_variable.c
@@ -15,32 +15,20 @@
 char	*var_extractor(char *input, int *i)
 {
     int start = *i;
-    int is_quote = false;
     char *result;
 
-    if (input[*i] == '(')
-    {
-        (*i)++;
-        skip_spases(input, i);
-        start = *i;
-        is_quote = true;
-    }
     while (input[*i])
 	{
-        if (is_quote && input[*i] == ' ')
-        {
-            result = ft_substr(input ,start, *i - start);
-            skip_spases(input, i);
-            if (input[*i] == ')')
-                return result;
-        }
-        else if (input[*i] == ' ' || input[*i] == '\"' || input[*i] == ')' 
+        if (input[*i] == ' ' || input[*i] == '\"' || input[*i] == ')' 
             || input[*i] == '$' || input[*i] == '\'' || input[*i] == '/'
             || input[*i] == 46 || input[*i] == '?')
             break;
 		(*i)++;
 	}
-    return ft_substr(input ,start, *i - start);
+    result = ft_substr(input ,start, *i - start);
+    if (!result)
+        return NULL;
+    return result;
 }
 
 bool is_there_valid_var(char *str)
@@ -82,19 +70,6 @@ char *get_word_from_quotes(char *input)
     }
     res[i] = '\0';
     return res;
-}
-
-int	write_str_without_end(char *dest, char *src, int *i)
-{
-    int len = 0;
-
-	while (src[len])
-	{
-		dest[*i] = src[len];
-		(*i)++;
-        len++;
-	}
-	return (len);
 }
 
 char *get_status_var(int status)
@@ -152,18 +127,6 @@ char *get_env_var(t_envp *envp, char *var)
         if (!result)
             return NULL;
     }
-        
-    return result;
-}
-char *get_expanded_var(t_app *shell, char *var)
-{
-    char *result;
-
-    if (!var)
-        return NULL;
-    result = execut_subcommand(shell, var);
-    if (!result)
-        return NULL;
     return result;
 }
 
@@ -178,7 +141,15 @@ char *expand_words(t_app *shell, char *input, int *i)
     if (input[*i] == ' ' || input[*i] == '\0' ||  input[*i] == '\"' || input[*i] == '/'
         || input[*i] == '$' || input[*i] == '\t' || input[*i] == '\'')
         result = ft_substr(input, *i - 1 , 1);
-    else if (input[*i] != '(' && input[*i] != '?')
+    
+    if (input[*i] == '?')
+    {
+        (*i)++;
+        result = get_status_var(shell->last_exit_code);
+        if (!result)
+            return ft_strdup("");
+    }
+    else if ((input[*i] >= 64 && input[*i] <= 90) || (input[*i] >= 97 && input[*i] <= 122))
     {
         var = var_extractor(input, i);
         if (var)
@@ -189,24 +160,5 @@ char *expand_words(t_app *shell, char *input, int *i)
             free(var);
         }
     }
-    else if (input[*i] == '(')
-    {
-        var = var_extractor(input, i);
-        if (var)
-        {
-            result = get_expanded_var(shell, var);
-            if (!result)
-                return ft_strdup("");
-            free(var);
-            (*i)++;
-        }
-    }
-    else if (input[*i] == '?')
-    {
-        (*i)++;
-        result = get_status_var(shell->last_exit_code);
-        if (!result)
-            return ft_strdup("");
-    } 
     return result;
 }
