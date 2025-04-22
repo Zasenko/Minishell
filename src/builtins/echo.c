ec969a225acf946b101b9ec45b63f706cd0c38a4
Dmitry Zasenko <dzasenko@c3r2p5.42vienna.com>
2025-04-22 12:29:59 +0200

    echo norm

--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -12,50 +12,48 @@
 
 #include "../../includes/minishell.h"
 
-int ft_echo(char **args, int fd)
+void	echo_check_args(char **args, int *i, bool *with_new_line)
 {
-    int i;
-    bool with_new_line;
+	int	d;
 
-    with_new_line = true;
-    if (!args || !*args)
-    {
-        ft_putstr_fd("\n", fd);
-        return (EXIT_SUCCESS);
-    }
-    
-    i = 1;
-    while (args[i])
-    {
-        if (!ft_strncmp(args[i], "-n", 2))
-        {
-            int d = 2;
-            while (args[i][d] && args[i][d] == 'n')
-            {
-                d++;
-            }
-            if (args[i][d] == '\0') {
-                with_new_line = false;
-                i++;
-            }
-            else {
-                break;
-            }
-        }
-        else {
-            break;
-        }
-    }
-    while (args[i])
-    {
-        ft_putstr_fd(args[i], fd);
-        if (args[i + 1] != NULL)
-            ft_putstr_fd(" ", fd);
-        i++;   
-    }
-    if (with_new_line == true)
-    {
-        ft_putstr_fd("\n", fd);
-    }
-    return (EXIT_SUCCESS);
+	while (args[*i])
+	{
+		if (!ft_strncmp(args[*i], "-n", 2))
+		{
+			d = 2;
+			while (args[*i][d] && args[*i][d] == 'n')
+				d++;
+			if (args[*i][d] == '\0')
+			{
+				*with_new_line = false;
+				*i = *i + 1;
+			}
+			else
+				break;
+		}
+		else
+			break;
+	}
+}
+
+int	ft_echo(char **args, int fd)
+{
+	int		i;
+	bool	with_new_line;
+
+	with_new_line = true;
+	if (!args || !*args)
+		return (ft_putstr_fd("\n", fd), EXIT_SUCCESS);
+	i = 1;
+	echo_check_args(args, &i, &with_new_line);
+	while (args[i])
+	{
+		ft_putstr_fd(args[i], fd);
+		if (args[i + 1] != NULL)
+			ft_putstr_fd(" ", fd);
+		i++;
+	}
+	if (with_new_line == true)
+		ft_putstr_fd("\n", fd);
+	return (EXIT_SUCCESS);
 }
